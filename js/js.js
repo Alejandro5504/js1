04a4667da963e4efecf75faaf253f6ab0713fbe6
Alejandro5504 <siseo15@hotmail.com>
2025-05-16 09:08:51 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,27 @@
+function calcularTest() {
+
+document.getElementById("p1").innerHTML="Tu orden de prioridades";
+document.getElementById("vaca").innerHTML="Tu Carrera";
+document.getElementById("tigre").innerHTML="El Orgullo";
+document.getElementById("oveja").innerHTML="El Amor";
+document.getElementById("caballo").innerHTML="La Familia";
+document.getElementById("mono").innerHTML="El Dinero";
+
+document.getElementById("p2").innerHTML="Como ves la vida:";
+document.getElementById("perro").innerHTML="Yo Soy";
+document.getElementById("gato").innerHTML="Mi Pareja";
+document.getElementById("rata").innerHTML="Lo que más odio";
+document.getElementById("cafe").innerHTML="Para mi el sexo es";
+document.getElementById("mar").innerHTML="Y la vida";
+
+
+document.getElementById("p3").innerHTML="Tu gente:";
+document.getElementById("amarillo").innerHTML="Nunca te olvidara: ";
+document.getElementById("naranja").innerHTML="Puede ser tu gran amigo/a: ";
+document.getElementById("blanco").innerHTML="Tu alma gemela: ";
+document.getElementById("rojo").innerHTML="Realmente amas a: ";
+document.getElementById("verde").innerHTML="Siempre estará en tu memoria: ";
+
+
+
+}
